22e09ff5f8c71631aa944afaace482e5e2820da0
KonTroAl <kostya98295@mail.ru>
2022-08-08 18:55:48 +0300

    done with menu

--- todo_list/frontend/src/components/users.js
+++ todo_list/frontend/src/components/users.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserItem = ({user}) => {
     return (
-        <div class='user_list'>
+        <div class='user_item'>
             <div>
                 {user.username}
             </div>
@@ -21,7 +21,7 @@ const UserItem = ({user}) => {
 
 const UserList = ({users}) => {
     return (
-        <div class='container'>
+        <div>
             <div class='user_headers'>
                 <div>
                     Username
@@ -36,7 +36,9 @@ const UserList = ({users}) => {
                     Email
                 </div>
             </div>
-            {users.map((user) => <UserItem user={user} />)}
+            <div class='user_list'>
+                {users.map((user) => <UserItem user={user} class='user_list' />)}
+            </div>
         </div>
     )
 }
