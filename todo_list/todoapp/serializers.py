fdd50452c852f2b571077b2c1134220df3295921
KonTroAl <kostya98295@mail.ru>
2022-08-10 16:07:46 +0300

    the start of work under serializers

---
+++ todo_list/todoapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+from .models import Project, ToDo
+
+class ProjectModelSerializer(ModelSerializer):
+    pass
+
+class TodoModelSerializer(ModelSerializer):
+    pass
+
