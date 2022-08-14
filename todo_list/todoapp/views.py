10b69dcb3d743b356b9c0fc2a7536b7466080272
KonTroAl <kostya98295@mail.ru>
2022-08-14 13:51:13 +0300

    add pagination

--- todo_list/todoapp/views.py
+++ todo_list/todoapp/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, ToDo
 from .serializers import ProjectHyperlinkedModelSerializer, ToDoModelSerializer
 
+
 # Create your views here.
+
+# Model == Project
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectHyperlinkedModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+
+
+# Model == To_Do
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
