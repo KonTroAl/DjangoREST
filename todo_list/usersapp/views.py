7eec57c47139f1ab2fc5d324eeeded1c1c140735
KonTroAl <kostya98295@mail.ru>
2022-08-15 10:59:26 +0300

    done with Users

--- todo_list/usersapp/views.py
+++ todo_list/usersapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
+from rest_framework import mixins
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+
 
 # Create your views here.
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
-
