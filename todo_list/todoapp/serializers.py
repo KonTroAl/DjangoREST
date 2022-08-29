961f3cb0ef24f1d1ecb79874aefadb57a3c7bf60
KonTroAl <troshenkin.k@yandex.ru>
2022-08-29 09:15:38 +0500

    done with APITestCase

--- todo_list/todoapp/serializers.py
+++ todo_list/todoapp/serializers.py
@@ -9,9 +9,16 @@ class ProjectModelSerializer(ModelSerializer):
         model = Project
         fields = '__all__'
 
-class ToDoModelSerializer(ModelSerializer):
+class ToDoModelSerializerBase(ModelSerializer):
 
     class Meta:
         model = ToDo
         exclude = ('is_active',)
 
+
+class ToDoModelSerializer(ModelSerializer):
+    project = ProjectModelSerializer()
+
+    class Meta:
+        model = ToDo
+        exclude = ('is_active',)
--- todo_list/todoapp/tests.py
+++ todo_list/todoapp/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 import json
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APITestCase
 from mixer.backend.django import mixer
 from usersapp.models import User
 from .views import ProjectModelViewSet, ToDoModelViewSet
@@ -47,17 +47,17 @@ class TestProjectViewSet(TestCase):
     #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     #APIClient
-    def test_project_detail(self):
+    def test_client_project_detail(self):
         client = APIClient()
         response = client.get(f'{self.url}{self.project.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_project_edit_guest(self):
+    def test_client_project_edit_guest(self):
         client = APIClient()
         response = client.put(f'{self.url}{self.project.id}/',self.project_dict_fake, format=self.format)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_project_edit_admin(self):
+    def test_client_project_edit_admin(self):
         client = APIClient()
         client.login(username=self.username, password=self.password)
         response = client.put(f'{self.url}{self.project.id}/',self.project_dict_fake, format=self.format)
@@ -65,6 +65,38 @@ class TestProjectViewSet(TestCase):
         self.project.refresh_from_db()
         self.assertEqual(self.project.project_name, self.project_dict_fake['project_name'])
         client.logout()
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestToDoViewSet(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = '/api/todo/'
+        self.username = 'KonTroAl'
+        self.password = 'Rjycnfynby1234567'
+        self.admin = User.objects.create_superuser(self.username, 'troshenkin.k@yandex.ru', self.password)
+        self.project_dict = {'project_name': 'Project', 'users': [self.admin.id]}
+        self.project_dict_fake = {'project_name': 'Fake', 'users': [self.admin.id]}
+        self.project = Project.objects.create(project_name='Project')
+        self.project.users.add(self.admin)
+        self.todo = ToDo.objects.create(project=self.project, text='text', user=self.admin)
+        self.todo_dict_fake = {'project': self.project.id, 'text': 'text_2', 'user': self.admin.id}
+
+        self.format = 'json'
+
+    def test_testcase_todo_get_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_testcase_todo_edit(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.put(f'{self.url}{self.todo.id}/', self.todo_dict_fake)
+        # response = self.client.get(f'{self.url}{self.todo.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.todo.refresh_from_db()
+        self.assertEqual(self.todo.text, self.todo_dict_fake['text'])
 
     def tearDown(self) -> None:
         pass
